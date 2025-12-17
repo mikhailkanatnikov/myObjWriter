2d4798e729b67e944130bac03c9b0594727c27b6
Mikhail_Kanatnikov <kanatnikovmihan@mail.ru>
2025-12-17 19:57:01 +0300

    .

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args){
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        try (PrintWriter printWriter = new PrintWriter("C:\\Users\\Asus\\Desktop\\УЧОБА\\ВУЗ_2 курс\\компГафика\\CGVSU-main\\Task2\\myObjWriter\\src\\main\\testOutput.txt")) {
+            String text = "попка";
+            printWriter.println(text);
+        }
     }
 }
--- src/main/java/writer/objWriter.java
+++ src/main/java/writer/objWriter.java
@@ -1,4 +1,27 @@
 package writer;
 
+import math.Vector3f;
+import model.Model;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class objWriter {
+
+    public String filePath = "C:\\Users\\Asus\\Desktop\\УЧОБА\\ВУЗ_2 курс\\компГрафика\\CGVSU-main\\Task2\\myObjWriter\\src\\main\\testOutput.txt";
+
+
+    public void write(Model model, String path) throws IOException {
+
+        //вершины v
+        for (Vector3f v : model.vertices) {
+            try (PrintWriter writer = new PrintWriter(filePath)) {
+
+            }
+
+
+        }
+
+
+    }
 }
