e03ba6d43c89f866ae6ba53885eae6c28b586617
Mikhail_Kanatnikov <kanatnikovmihan@mail.ru>
2025-12-17 18:39:54 +0300

    импорт всех классов и пакетов

---
+++ src/main/java/math/Vector2f.java
@@ -0,0 +1,11 @@
+package math;
+
+
+public class Vector2f {
+    public Vector2f(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    float x, y;
+}
---
+++ src/main/java/model/Model.java
@@ -0,0 +1,15 @@
+package model;
+
+
+import math.Vector3f;
+import math.Vector2f;
+import java.util.ArrayList;
+
+
+public class Model {
+
+    public ArrayList<Vector3f> vertices = new ArrayList<Vector3f>();
+    public ArrayList<Vector2f> textureVertices = new ArrayList<Vector2f>();
+    public ArrayList<Vector3f> normals = new ArrayList<Vector3f>();
+    public ArrayList<Polygon> polygons = new ArrayList<Polygon>();
+}
